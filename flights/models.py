474326b935b8514307ebb486d5242fc6c59d6322
kishore-g-16 <kishore@dataterrain.com>
2025-04-04 14:34:08 +0530

    added timing

--- flights/models.py
+++ flights/models.py
@@ -15,8 +15,14 @@ class Flight(models.Model):
                      ("Business", "Business")
                      ])
 
+    departure_time = models.DateTimeField()  # New field for departure time
+    arrival_time = models.DateTimeField()  # New field for arrival time
+
     def __str__(self):
-        return f"{self.airline.name} - {self.flight_number}"
+        departure_time_str = self.departure_time.strftime('%Y-%m-%d %H:%M') \
+            if self.departure_time else "No Departure Time"
+        return f"{self.airline.name} - {self.flight_number} ({departure_time_str})"
+
 
 class Booking(models.Model):
     CLASS_CHOICES = [
@@ -27,6 +33,8 @@ class Booking(models.Model):
     flight = models.ForeignKey(Flight, on_delete=models.CASCADE) # ForeignKey to Flight
     seat_class = models.CharField(max_length=20, choices=CLASS_CHOICES)
     number_of_seats = models.PositiveIntegerField()
+    booking_time = models.DateTimeField()  # New field for selecting booking time
 
     def __str__(self):
-        return f"{self.flight} - {self.passenger_name} - {self.number_of_seats} - {self.seat_class}"
+        booking_time_str = self.booking_time.strftime('%Y-%m-%d %H:%M') if self.booking_time else "No Booking Time"
+        return f"{self.flight} - {self.passenger_name} - {self.number_of_seats} - {self.seat_class} - {booking_time_str}"
